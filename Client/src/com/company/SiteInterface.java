374755f0b800a4f77c009517dce701d52c8ed8e3
Dashtikh <56628256+Dashtikh@users.noreply.github.com>
2020-01-24 11:55:57 +0330

    code completed

---
+++ Client/src/com/company/SiteInterface.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface SiteInterface {
+    void save(String user , String password) throws Exception;
+    void update(String user , String password) throws Exception;
+}
