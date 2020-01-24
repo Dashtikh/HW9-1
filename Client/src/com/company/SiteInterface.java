715fe4b0133ee1fd77440f2ddec7dddcb54c4cc0
Dashtikh <56628256+Dashtikh@users.noreply.github.com>
2020-01-24 12:17:38 +0330

    code completed

--- Client/src/com/company/SiteInterface.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface SiteInterface {
-    void save(String user , String password) throws Exception;
-    void update(String user , String password) throws Exception;
-}
--- Server/src/com/company/Main.java
+++ Server/src/com/company/Main.java
@@ -11,12 +11,14 @@ public class Main {
     public static void main(String[] args) {
         try {
             LocateRegistry.createRegistry(1099);
-            System.setProperty("java.rmi.server.hostname","192.168.1.110");
+            System.setProperty("java.rmi.server.hostname","192.168.1.104");
 
             infosaver infosaver = new infosaver();
             Naming.rebind("site", infosaver);
             System.out.println("server is ...");
         } catch (RemoteException | MalformedURLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
